fad1c5ee8ad24451ee9e062c8afe52bb53bd8883
Edwin A. Trivinos R <edwin.trivinos@hotmail.com>
2016-08-05 11:34:34 -0500

    Split master/detail views

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,9 +1,6 @@
 import { Component } from '@angular/core';
-
-export class Hero {
-  id: number;
-  name: string;
-}
+import { Hero } from './hero';
+import { HeroDetailComponent } from './hero-detail.component';
 
 const HEROES: Hero[] = [
   { id: 11, name: 'Mr. Nice' },
@@ -22,19 +19,6 @@ const HEROES: Hero[] = [
   selector: 'my-app',
    template: `
     <h1>{{title}}</h1>
-    
-    <div *ngIf="selectedHero">
-      <h2>{{selectedHero.name}} details!</h2>
-      <div>
-        <label>id: </label>
-        {{selectedHero.id}}
-      </div>
-      
-      <div>
-        <label>name: </label>
-        <input [(ngModel)]="selectedHero.name" placeholder="name"/>
-      </div>
-    </div>
 
     <h2>My Heroes</h2>
     <ul class="heroes">
@@ -44,7 +28,10 @@ const HEROES: Hero[] = [
           <span class="badge">{{hero.id}}</span> {{hero.name}}
       </li>
     </ul>
+
+    <my-hero-detail [hero]="selectedHero"></my-hero-detail>
     `,
+    directives: [HeroDetailComponent],
     styles: [`
       .selected {
         background-color: #CFD8DC !important;
