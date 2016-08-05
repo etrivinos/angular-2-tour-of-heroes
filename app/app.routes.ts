3fac29efa5601723fd131b58e931c0600189fd8c
Edwin A. Trivinos R <edwin.trivinos@hotmail.com>
2016-08-05 13:19:51 -0500

    Finish the app routing

--- app/app.routes.ts
+++ app/app.routes.ts
@@ -1,6 +1,7 @@
 import { provideRouter, RouterConfig }  from '@angular/router';
 import { DashboardComponent } from './dashboard.component';
 import { HeroesComponent } from './heroes.component';
+import { HeroDetailComponent } from './hero-detail.component';
 
 const routes: RouterConfig = [
   {
@@ -10,6 +11,10 @@ const routes: RouterConfig = [
   {
     path: 'heroes',
     component: HeroesComponent
+  },
+  {
+    path: 'detail/:id',
+    component: HeroDetailComponent
   },
   {
     path: '',
