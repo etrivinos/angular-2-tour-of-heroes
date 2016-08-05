45e22b101a91bd411b57e7ac521ed72ef0aec33f
Edwin A. Trivinos R <edwin.trivinos@hotmail.com>
2016-08-05 13:56:05 -0500

    Add CRUD to heroes

--- app/heroes.component.ts
+++ app/heroes.component.ts
@@ -1,18 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Hero } from './hero';
+import { HeroDetailComponent } from './hero-detail.component';
 
 import { HeroService } from './hero.service';
 
 @Component({
   selector: 'my-heroes',
   templateUrl: 'app/heroes.component.html',
-  styleUrls: ['app/heroes.component.css']
+  styleUrls: ['app/heroes.component.css'],
+  directives: [HeroDetailComponent]
 })
 export class HeroesComponent implements OnInit { 
   title = 'Tour of Heroes';
   heroes: Hero[];
   selectedHero : Hero;
+  error: any;
+  addingHero: boolean = false;
 
   constructor(private heroService: HeroService, private router: Router) {}
 
@@ -31,5 +35,26 @@ export class HeroesComponent implements OnInit {
 
   gotoDetail() {
     this.router.navigate(['/detail', this.selectedHero.id]);
+  }
+
+  addHero() {
+    this.addingHero = true;
+    this.selectedHero = null;
+  }
+
+  close(savedHero: Hero) {
+    this.addingHero = false;
+    if (savedHero) { this.getHeroes(); }
+  }
+
+  deleteHero(hero: Hero, event: any) {
+    event.stopPropagation();
+    this.heroService
+        .delete(hero)
+        .then(res => {
+          this.heroes = this.heroes.filter(h => h !== hero);
+          if (this.selectedHero === hero) { this.selectedHero = null; }
+        })
+        .catch(error => this.error = error);
   }
 }
